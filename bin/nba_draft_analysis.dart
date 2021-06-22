3df68428da4332c5cb8cb54d7e51b2fed8f13947
Aditya Kishore <akushwarrior@gmail.com>
2021-06-21 21:25:25 -0700

    RAPTOR-based draft stuff (actually good this time)

--- bin/nba_draft_analysis.dart
+++ bin/nba_draft_analysis.dart
@@ -22,34 +22,49 @@ double raptorWeight = 1.0;
 
 void main(List<String> arguments) {
   var startYear = 2002;
-  var finalYear = 2016;
-  var firstNYears = 5;
+  var finalYear = 2019;
 
   var year = startYear;
 
   var raptorCsv = File('stats/RAPTOR/raptor.csv');
-  var listRowsRaptor = CsvToListConverter(eol: '\n').convert(raptorCsv.readAsStringSync());
+  var listRowsRaptor =
+      CsvToListConverter(eol: '\n').convert(raptorCsv.readAsStringSync());
   for (var i = 1; i < listRowsRaptor.length; i++) {
     var row = listRowsRaptor[i];
     var season = Season(row[0] as String, (row[2] as num).toInt() - 1);
     seasonToRaptorWA[season] = (row[9] as num).toDouble();
   }
 
+  var teamCSV = File('stats/team/all_seasons.csv');
+  var listRowsTeams =
+      CsvToListConverter(eol: '\n').convert(teamCSV.readAsStringSync());
+
   while (year <= finalYear) {
     var draftCsv = File('stats/draft/$year.csv');
-    var listRows = CsvToListConverter(eol: '\n').convert(draftCsv.readAsStringSync());
+    var listRows =
+        CsvToListConverter(eol: '\n').convert(draftCsv.readAsStringSync());
     var draftSpot = 1;
     while (draftSpot < listRows.length) {
       var playerRow = listRows[draftSpot];
       if (playerRow[0] == 'pk') continue;
-      var player = Player(playerRow[2] as String, {});
+      var player = Player(playerRow[2] as String, {}, playerRow[1] as String);
       var draftSpotPlayers = pickToPlayers[draftSpot];
       if (draftSpotPlayers == null) {
         pickToPlayers[draftSpot] = {player};
       } else {
         pickToPlayers[draftSpot].add(player);
       }
-      for (var i = year; i < year + firstNYears; i++) {
+      for (var i = year;; i++) {
+        var relevantSeasons = listRowsTeams.where((List elem) {
+          return ((elem[1] as String) == player.name) &&
+              '$i-' + ('${i + 1}').substring(2) ==
+                  (elem[elem.length - 1] as String);
+        }).toList();
+        try {
+          if (relevantSeasons.first[2] != player.draftedTeam) break;
+        } catch (_) {
+          break;
+        }
         var rwa = seasonToRaptorWA[Season(player.name, i)];
         rwa = rwa ?? 0;
 
@@ -61,36 +76,6 @@ void main(List<String> arguments) {
     year++;
   }
 
-
-  double standard;
-  double minWAR = 100;
-  for (var spot in pickToPlayers.keys) {
-    var playersAtSpot = pickToPlayers[spot];
-    var totalWAR = 0.0;
-
-    for (var player in playersAtSpot) {
-      var valueList = player.yearToWinsAdded.values.toList();
-      for (var i = 0; i < firstNYears; i++) {
-        totalWAR += valueList[i];
-      }
-    }
-
-    double output;
-    if (standard == null) {
-      output = 100;
-      standard = totalWAR;
-    } else {
-      output = totalWAR/standard*100;
-    }
-    if (output < minWAR) {
-      minWAR = output;
-    }
-  }
-
-  minWAR = minWAR > 0 ? minWAR : -minWAR;
-  standard += minWAR;
-
-
   var table = <List<String>>[];
   for (var spot in pickToPlayers.keys) {
     var playersAtSpot = pickToPlayers[spot];
@@ -98,20 +83,13 @@ void main(List<String> arguments) {
 
     for (var player in playersAtSpot) {
       var valueList = player.yearToWinsAdded.values.toList();
-      for (var i = 0; i < firstNYears; i++) {
+      for (var i = 0; i < valueList.length; i++) {
         totalWAR += valueList[i];
       }
     }
 
-    double output;
-    if (spot == 1) {
-      output = 100;
-    } else {
-      output = (totalWAR)/standard*100 + minWAR;
-    }
-    table.add(['$spot', formatDouble(output)]);
+    table.add(['$spot', formatDouble(totalWAR)]);
   }
-
 
   var bracketTable = <List<String>>[];
   double standard2;
@@ -124,7 +102,7 @@ void main(List<String> arguments) {
 
     for (var player in playersAtSpot) {
       var valueList = player.yearToWinsAdded.values.toList();
-      for (var i = 0; i < firstNYears; i++) {
+      for (var i = 0; i < valueList.length; i++) {
         totalWAR += valueList[i];
       }
     }
@@ -134,29 +112,33 @@ void main(List<String> arguments) {
       standard2 = totalWAR;
       bracketTable.add(['1', '100', formatDouble(totalWAR)]);
     } else {
-      output = totalWAR/standard2*100;
+      output = totalWAR / standard2 * 100;
       cumulatedRelative += output;
       cumulatedWAR += totalWAR;
     }
 
-
     if (spot != 5 && spot % 5 == 0) {
       cumulatedRelative /= 5;
       cumulatedWAR /= 5;
-      bracketTable.add(['${spot-4}-$spot', formatDouble(cumulatedRelative), formatDouble(cumulatedWAR)]);
+      bracketTable.add([
+        '${spot - 4}-$spot',
+        formatDouble(cumulatedRelative),
+        formatDouble(cumulatedWAR)
+      ]);
       cumulatedRelative = 0;
     } else if (spot == 5) {
       cumulatedRelative /= 4;
       cumulatedWAR /= 4;
-      bracketTable.add(['2-5', formatDouble(cumulatedRelative), formatDouble(cumulatedWAR)]);
+      bracketTable.add(
+          ['2-5', formatDouble(cumulatedRelative), formatDouble(cumulatedWAR)]);
       cumulatedRelative = 0;
     }
   }
   // Output spot values to console.
-  print(tabulate(table, ['Draft Position', 'Value (0-100)']));
+  print(tabulate(table, ['Draft Position', 'Value (WAR Produced With First Team)']));
 
   // Output spot values to CSV.
-  table.insert(0, ['Draft Position', 'Value (0-100)']);
+  table.insert(0, ['Draft Position', 'Value (WAR Produced With First Team)']);
   var csv = table.map((e) => e.join(',')).join('\n');
   var outputFile = File('output/values.csv');
   if (!outputFile.existsSync()) {
@@ -165,18 +147,23 @@ void main(List<String> arguments) {
   outputFile.writeAsStringSync(csv);
 
   // Output range averages to console.
-  print(tabulate(bracketTable, ['Draft Position(s)', 'Avg. Value (0-100)', 'Avg. WAR after $firstNYears yrs.']));
+  print(tabulate(bracketTable, [
+    'Draft Position(s)',
+    'Avg. Value (0-100)',
+    'Avg. WAR After First Contract'
+  ]));
 }
 
-String formatDouble (double base) {
+String formatDouble(double base) {
   return '$base'.length > 7 ? '$base'.substring(0, 7) : '$base';
 }
 
 class Player {
   String name;
   Map<int, double> yearToWinsAdded;
+  String draftedTeam;
 
-  Player(this.name, this.yearToWinsAdded);
+  Player(this.name, this.yearToWinsAdded, this.draftedTeam);
 
   @override
   bool operator ==(Object other) {
